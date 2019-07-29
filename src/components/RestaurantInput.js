b2d5798f3501ae297065ba24326be5d644e22b70
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-29 15:00:04 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
 
-export class RestaurantInput extends Component {
+class RestaurantInput extends Component {
 
   state = {
     name: '',
@@ -50,13 +50,6 @@ export class RestaurantInput extends Component {
   }
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addRestaurant: (restaurant) => {
-      dispatch(addRestaurant(restaurant))
-    }
-  };
-
 
 //connect this component by wrapping RestaurantInput below
-export default connect(null, mapDispatchToProps)(RestaurantInput);
+export default connect(null, { addRestaurant })(RestaurantInput);
